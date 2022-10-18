ba9db7b6d721186f4de76552b4cd9dc7b696b7b5
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-10-18 03:19:41 -0700

    setup commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <header>
+           <a href="/">Amazona</a>
+        </header>
+        <main>
+            List Products
+        </main>
+    </div>
+  );
+}
+
+export default App;

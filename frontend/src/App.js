6cd038efac68afaa3752562d7abcca8fc7d461ea
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-10-19 04:02:23 -0700

    installing react-bootsrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,39 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 import ProductScreen from "./screens/ProductScreen";
+import {LinkContainer} from 'react-router-bootstrap'
 
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to='/'>
-              <h3>Amazona</h3>
-          </Link>
+          <Navbar bg='dark' varient="dark">
+             <Container>
+                <LinkContainer to='/'>
+                   <Navbar.Brand className="text-white">Amazona</Navbar.Brand>
+                </LinkContainer>
+                
+             </Container>
+          </Navbar>
         
         </header>
         <main>
+        <Container>
           <Routes>
             <Route path="/product/:slug" element={<ProductScreen />}/>
               <Route path="/" element={<HomeScreen />} />
           </Routes>
         
+        </Container>
         </main>
+        <footer className="bg-dark p-5">
+            <div className="text-center text-white">All right reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );

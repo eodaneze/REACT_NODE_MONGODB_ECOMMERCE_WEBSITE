011a68ebf6d1d1b282d0154365f616e3b2349b76
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-10-21 01:21:30 -0700

    connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected successfully');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 const app = express();
 // test
 app.get('/api/products', (req, res) => {

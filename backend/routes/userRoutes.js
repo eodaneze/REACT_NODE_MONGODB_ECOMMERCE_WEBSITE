240de8384bad2c24657ab6b716d6a55f2ebe1fb1
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-10-28 06:39:15 -0700

    creating signin-backend-API

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import expressAsyncHandler from 'express-async-handler'
+import { generateToken } from "../utlis.js";
+
+const userRouter = express.Router();
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: userRouter.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user)
+        });
+        // return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+export default userRouter
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import data from './data.js';
-import dotenv from 'dotenv'
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
+import express from "express";
+import mongoose from "mongoose";
+import data from "./data.js";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected successfully');
+    console.log("connected successfully");
   })
   .catch((err) => {
     console.log(err.message);
   });
 const app = express();
 
-app.use('/api/seed', seedRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/seed", seedRouter);
 // test
-app.use('/api/products', productRouter)
+app.use("/api/products", productRouter);
+app.use("/api/users", userRoutes);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
